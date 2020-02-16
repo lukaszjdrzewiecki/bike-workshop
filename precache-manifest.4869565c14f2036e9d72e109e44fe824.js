fe64270dece485fe98bec2729bb5ff53ed962b2f
Filip Drzewiecki <filip.drzewiecki@jit.team>
2020-02-16 11:56:12 +0100

    Updates

---
+++ precache-manifest.4869565c14f2036e9d72e109e44fe824.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e1c5f20b3708571332c34bf2b30efa",
+    "url": "/bike-workshop/index.html"
+  },
+  {
+    "revision": "e4cd2cbe7bd2a1d4e00d",
+    "url": "/bike-workshop/static/css/main.afeede5b.chunk.css"
+  },
+  {
+    "revision": "244030514670fda24477",
+    "url": "/bike-workshop/static/js/2.3c59f30c.chunk.js"
+  },
+  {
+    "revision": "e4cd2cbe7bd2a1d4e00d",
+    "url": "/bike-workshop/static/js/main.0a932d90.chunk.js"
+  },
+  {
+    "revision": "bf388cf01ea4938aa5a6",
+    "url": "/bike-workshop/static/js/runtime-main.ace27181.js"
+  },
+  {
+    "revision": "f90acdbb677cbda753a4f5716c012bca",
+    "url": "/bike-workshop/static/media/BikeLogo.f90acdbb.png"
+  },
+  {
+    "revision": "a132c81371a89d7ec4d8f40226e35efd",
+    "url": "/bike-workshop/static/media/CampagnoloFreewheel.a132c813.jpeg"
+  },
+  {
+    "revision": "f99c618c7daae725a679bb3845a00b42",
+    "url": "/bike-workshop/static/media/EC.f99c618c.jpg"
+  },
+  {
+    "revision": "1b135ffc1188c87fd103eee65e292f25",
+    "url": "/bike-workshop/static/media/IS.1b135ffc.jpg"
+  },
+  {
+    "revision": "ed9df5515c76f600425e9c6b2f037b72",
+    "url": "/bike-workshop/static/media/ShimanoFreewheel.ed9df551.jpeg"
+  },
+  {
+    "revision": "bbeb490be514e643db018da67c77abc4",
+    "url": "/bike-workshop/static/media/ShimanoSplineFreewheel.bbeb490b.jpeg"
+  },
+  {
+    "revision": "ab16bd298744a4414574173c0a45b316",
+    "url": "/bike-workshop/static/media/SramFreewheel.ab16bd29.jpg"
+  },
+  {
+    "revision": "a9b396f2c20a8c9b88b9af85674381ad",
+    "url": "/bike-workshop/static/media/Thread.a9b396f2.jpg"
+  },
+  {
+    "revision": "f0115a1d4bd4c2b44fcef37a293d1d6f",
+    "url": "/bike-workshop/static/media/ZS.f0115a1d.jpg"
+  },
+  {
+    "revision": "8a553af53418b5d5ef1f30243906004f",
+    "url": "/bike-workshop/static/media/companyLogo.8a553af5.png"
+  },
+  {
+    "revision": "c8e608328b90498fc4f765d72018ca77",
+    "url": "/bike-workshop/static/media/drivetrain.c8e60832.png"
+  },
+  {
+    "revision": "408c5607bd1223c77379aa711367b657",
+    "url": "/bike-workshop/static/media/frame.408c5607.png"
+  },
+  {
+    "revision": "76ea7450da4b272d5a4a3f248355ca3a",
+    "url": "/bike-workshop/static/media/hub.76ea7450.png"
+  },
+  {
+    "revision": "be36551f8503be8532b51b1a2ea39b4d",
+    "url": "/bike-workshop/static/media/wheel.be36551f.png"
+  }
+]);
